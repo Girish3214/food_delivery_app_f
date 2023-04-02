519d2bbbaebe2d21c55062beb6ed9f7304a53581
Girish <m.girishroy@gmail.com>
2023-04-02 23:14:41 +0530

    app icon font changes

--- lib/widgets/app_icon.dart
+++ lib/widgets/app_icon.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:food_delivery_app/utils/dimensions.dart';
 
 class AppIcon extends StatelessWidget {
   final IconData icon;
   final Color bgColor;
   final Color iconColor;
   final double size;
+  final double iconSize;
 
   const AppIcon({
     super.key,
@@ -13,6 +13,7 @@ class AppIcon extends StatelessWidget {
     this.bgColor = const Color(0xFFfcf4e4),
     this.iconColor = const Color(0xFF756d54),
     this.size = 40,
+    this.iconSize = 16,
   });
 
   @override
@@ -25,7 +26,7 @@ class AppIcon extends StatelessWidget {
       child: Icon(
         icon,
         color: iconColor,
-        size: Dimensions.iconSize16,
+        size: iconSize,
       ),
     );
   }
